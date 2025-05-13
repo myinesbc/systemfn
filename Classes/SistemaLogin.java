ef5b812e6f658892e15f297b6513b6dc6ae5de7a
Welterson <weltersongabriel.ti@gmail.com>
2025-05-13 17:04:02 -0300

    Update SistemaLogin.java

--- Classes/SistemaLogin.java
+++ Classes/SistemaLogin.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class App {
+public class SistemaLogin {
     private static List<Usuario> usuarios = new ArrayList<>();
 
     public static void main(String[] args) {
