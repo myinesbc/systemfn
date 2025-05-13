b4ca18ed61871fc59dc89fbe9e981b1a971a61db
Welterson <weltersongabriel.ti@gmail.com>
2025-05-13 14:52:58 -0300

    Update Cadastro.java

--- Classes/Cadastro.java
+++ Classes/Cadastro.java
@@ -1,3 +1,16 @@
-public class Cadastro {
-    // Inserir
+import java.util.List;
+
+public class Cadastro extends Usuario {
+
+    public Cadastro(String fullname, String idnumber, String passwoerd) {
+        super(fullname, idnumber, passwoerd);
+        //TODO Auto-generated constructor stub
+    }
+
+    public void cadastrar(List<Usuario> usuarios){
+        usuarios.add(this);
+        System.out.println("Usuário cadastrado com sucesso.");
+    }
+}
+
 }
