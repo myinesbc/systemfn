fc2ca70da4f647d6ec336033ee25c8346acb59f5
Welterson <weltersongabriel.ti@gmail.com>
2025-05-14 08:54:38 -0300

    Create Condicao.java

---
+++ Classes/Condicao.java
@@ -0,0 +1,12 @@
+public class Condicao {
+
+ protected String condicao;
+
+  public String getCondicao(){
+    return condicao;
+  }
+  public void setCondicao(String condicao){
+    this.condicao = condicao;
+  }
+
+}
