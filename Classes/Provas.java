d1d1dde50549e709bc28764252b7434f4cae2329
Welterson <weltersongabriel.ti@gmail.com>
2025-05-14 08:52:45 -0300

    Create Provas.java

---
+++ Classes/Provas.java
@@ -0,0 +1,27 @@
+public class Provas {
+  protected double a1;
+  protected double a2;
+  protected double a3;
+
+  public double getA1(){
+    return a1;
+  }
+  public void setA1(double a1){
+    this.a1 = a1;
+  }
+  
+  public double getA2(){
+    return a2;
+  }
+  public void setA2(double a2){
+    this.a2 = a2;
+  }
+  
+  public double getA3(){
+    return a3;
+  }
+  public void setA3(double a3){
+    this.a3 = a3;
+  }
+
+}
