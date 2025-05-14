061ea5fd24b9de4a71c86a50524c6b6e8ff6a299
Welterson <weltersongabriel.ti@gmail.com>
2025-05-14 08:53:45 -0300

    Create NotaTotal.java

---
+++ Classes/NotaTotal.java
@@ -0,0 +1,13 @@
+public class NotaTotal {
+
+ protected double notaTotal;
+ 
+  public void setNotaTotal(double notaTotal){
+    this.notaTotal = notaTotal;
+  }
+
+  public double getNotaTotal(){
+    return notaTotal;
+  }
+ 
+}
